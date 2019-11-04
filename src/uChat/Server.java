b9c3f18483cca268d2d756ba4a957cab14a3e73e
MrRaiN <mc.rain345@gmail.com>
2019-11-04 19:39:20 +0100

    initial commit

---
+++ src/uChat/Server.java
@@ -0,0 +1,53 @@
+package uChat;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Server
+ */
+@WebServlet("/Server")
+public class Server extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Server() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		ServletOutputStream out = response.getOutputStream();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"UTF-8\"/>");
+		out.println("<title>uChat</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h3>Hello World</h3>");
+		out.println("</body>");
+		out.println("<html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
