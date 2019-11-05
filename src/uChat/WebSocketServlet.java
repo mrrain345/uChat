4fe74ee0beaf422987d34d57fe673363db9ff3c4
MrRaiN <mc.rain345@gmail.com>
2019-11-05 18:45:58 +0100

    websocket

---
+++ src/uChat/WebSocketServlet.java
@@ -0,0 +1,33 @@
+package uChat;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/websocketendpoint")
+public class WebSocketServlet {
+
+	@OnOpen
+	public void open() {
+		System.out.println("WebSocket open");
+	}
+	
+	@OnClose
+	public void close() {
+		System.out.println("WebSocket close");
+	}
+	
+	@OnError
+	public void onError(Throwable error) {
+		System.out.println("WebSocket error");
+		error.printStackTrace();
+	}
+	
+	@OnMessage
+	public String handleMessage(String message) {
+		System.out.println("WebSocket message: " + message);
+		return message;
+	}
+}   
