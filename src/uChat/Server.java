be34ec1b3169e614e77cb4a9be346826f6980997
MrRaiN <mc.rain345@gmail.com>
2019-11-07 21:26:59 +0100

    database configuration

--- src/uChat/Server.java
+++ src/uChat/Server.java
@@ -1,34 +1,31 @@
 package uChat;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
-/**
- * Servlet implementation class Server
- */
 @WebServlet("/Server")
 public class Server extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Server() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
 		ServletOutputStream out = response.getOutputStream();
 		out.println("<html>");
@@ -38,15 +35,36 @@ public class Server extends HttpServlet {
 		out.println("</head>");
 		out.println("<body>");
 		out.println("<h3>Hello World</h3>");
+		
+		try {
+		
+			Context context = new InitialContext();
+			DataSource ds = (DataSource) context.lookup("java:/comp/env/jdbc/database");
+			
+			Class.forName("org.mariadb.jdbc.Driver");
+			Connection conn = ds.getConnection();
+			Statement db = conn.createStatement();
+			
+			String sql = "SELECT 1 as id";
+			ResultSet res = db.executeQuery(sql);
+			
+			while(res.next()) {
+				int id = res.getInt("id");
+				out.println("DB: " + id + "<br/>");
+			}
+			
+			res.close();
+			db.close();
+			conn.close();
+		} catch (NamingException | SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
 		out.println("</body>");
 		out.println("<html>");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
